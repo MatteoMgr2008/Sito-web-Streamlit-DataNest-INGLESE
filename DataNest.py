c987270ff842772a4b6aa80077d170b94a6e1db2
Matteo Magrino <145618533+MatteoMgr2008@users.noreply.github.com>
2025-05-23 11:55:35 +0200

    Add files via upload

--- DataNest.py
+++ DataNest.py
@@ -5,6 +5,29 @@ import plotly.express as px
 import altair as alt
 import numpy as np
 from scipy.stats import zscore
+import streamlit.components.v1 as components
+
+# Inietta lo script di Microsoft Clarity e di Google Tag per analisi comportamento utenti
+components.html("""
+<!-- Microsoft Clarity -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rnynbnc8ad");
+</script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6601SMNJT4"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6601SMNJT4');
+</script>
+""", height=0)
 
 st.title("DataNest: the smart place for smart data")
 
